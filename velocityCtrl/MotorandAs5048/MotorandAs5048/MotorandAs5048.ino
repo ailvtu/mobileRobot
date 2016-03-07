14e10509002cd3e18e11c11d68bc442bda705def
ailvtu <2442844656@qq.com>
2016-03-07 16:44:24 +0800

    add the convert function to lib

--- velocityCtrl/MotorandAs5048/MotorandAs5048/MotorandAs5048.ino
+++ velocityCtrl/MotorandAs5048/MotorandAs5048/MotorandAs5048.ino
@@ -4,78 +4,48 @@
 #include <PID_v1.h>
 
 MotorandAs5048  m;
-double Setpoint, Input, Output;
-double Kp=2, Ki=0, Kd=0;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-double sp1,sp2;
+
+//double Setpoint, Input, Output;
+//double Kp=2, Ki=0, Kd=0;
+//PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
 void setup(){
-  
-  Serial.begin(9600);
+ Serial.begin(9600);
   m.motorInit();
   m.setZeroReg();
+
+ 
+//  Input = velo1*10;
+//  Setpoint = 255;
+//   //turn the PID on
+//  myPID.SetMode(AUTOMATIC);
   
-  Input = analogRead(5);
-  Setpoint = 100;
-   //turn the PID on
-  myPID.SetMode(AUTOMATIC);
   // MsTimer2::set(100, timerGetSpeed); // 500ms period
   // MsTimer2::start();
-  sp1 = m.getSpeed(0);
+
+
   
 }
 void loop(){
-  
  m.goForwaed();
  //m.Brake( RightMotorBrake);
- GetSpeed(0);
- GetSpeed(1);
- myPID.Compute();
+/*
+ GetSpeed();
+ Serial.print("LV:");
+ Serial.println(velo1);
+  Serial.print("RV:");
+ Serial.println(velo2);
+ */
+ m.getSpeed();
+ Serial.print(m.velo1);
+// Input = 200;
+// myPID.Compute();
+// Serial.println(Output);
+// digitalWrite(LeftMotorDire,1);    
+// analogWrite(LeftMotorSpeed,Output);
  
   //analogWrite(6, Output);Serial.print("Output:" );Serial.println(Output);
  delay(1000);
 }
-void GetSpeed(int side){
-  
-  double errL,errR ;
- // Serial.print("L:");
- //  Serial.println(spL2);
-   /*
-   spL1 = m.getSpeed(1);
-   Serial.print("R:");
-   Serial.println(spL1);
-  */
-  //sp2 = m.getSpeed(1);
-  if(side == 0){
-    sp2 = m.getSpeed(0);
-    if(sp2<sp1)
-  { 
- errL = sp1- sp2;
-  }else
-  {
-  sp1 = sp1+360;
-  errL = sp1 - sp2;
-  }
-  Serial.print("errL:");
- Serial.println(errL);
-  ///*****////
-  }
-  
-  if(side ==1){
-    sp2 = m.getSpeed(1);
-    if(sp2>sp1)
-  { 
- errR = sp2 - sp1;
-  }else
-  {
-  sp2 = sp2+360;
-  errR = sp2 - sp1;
-  }
-  Serial.print("errR:");
- Serial.println(errR);
-  }
 
-  //*********/
 
- 
-  sp1 = sp2;
-}
