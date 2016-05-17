c6a8eb6fcae0581746e9a452945c37afe36b8b2e
ailvtu <2442844656@qq.com>
2016-05-17 16:24:31 +0800

    add a method of send data

--- PIDcontrol/PIDcontrol.ino
+++ PIDcontrol/PIDcontrol.ino
@@ -12,7 +12,7 @@ double sum,vel;
 MotorandAs5048  m;
 
 double Setpoint_R,Setpoint_L, Input_L, Output_L,Input_R, Output_R;
-double Kp=0.5, Ki=5, Kd=0;
+double Kp=0.5, Ki=7, Kd=0;
 PID myPID_L(&Input_L, &Output_L, &Setpoint_L, Kp, Ki, Kd, DIRECT);
 PID myPID_R(&Input_R, &Output_R, &Setpoint_R, Kp, Ki, Kd, DIRECT);
 
@@ -23,13 +23,25 @@ void setup(){
   m.setZeroReg();
   myPID_L.SetMode(AUTOMATIC);
   myPID_R.SetMode(AUTOMATIC);
+  //*****************
   Setpoint_R = setValue*9.21+12.6; //right
   Setpoint_L= setValue*7.03+40.55;//left
- // Setpoint = 70;
+  /****************/
+//Setpoint_R = 70;//right
+ // Setpoint_L= 70;
 }
 void loop(){
 
-  //***************
+  //*************
+PIDCtrol2();
+
+ // ***********/
+  
+/*********************************
+collectData();
+ *******************************/
+}
+void PIDCtrol(){
   
    Input_L= filter(10,0);
    Input_R = filter(10,1);
@@ -51,24 +63,50 @@ void loop(){
   Serial.print("   Output_R");
   Serial.println(Output_R);
   m.setSpeed(forward,forward,Output_L,Output_R);
- // ***********/
   
-  /*********************************
-  while(Setpoint<255){
-  m.setSpeed(forward,forward,Setpoint,0);
-  Input = filter(10);
-   Serial.print("Setpoint");
-  Serial.print(Setpoint);
-   Serial.print("  Input");
-  Serial.println(Input);
-  myPID.Compute();
-  delay(5000);
-  Setpoint=Setpoint+20;
-  if(Setpoint>255)
-          Setpoint=70;
-  }
-  ********************************/
 }
+void PIDCtrol2(){
+  
+   Input_L= filter(10,0);
+   Input_R = filter(10,1);
+  Serial.println(Setpoint_L);
+//  Serial.println(Setpoint_R);
+   //Input = Input*10; 
+   Input_R = 9.21*Input_R+12.65;
+   Input_L = 7.03*Input_L+40.55;
+  Serial.println(Input_L);
+  //Serial.println(Input_R);
+  myPID_L.Compute();
+  myPID_R.Compute();
+  Serial.println(Output_L);
+  //Serial.println(Output_R);
+  m.setSpeed(forward,forward,Output_L,Output_R);
+  
+}
+
+void collectData(){
+  while(Setpoint_L<255){
+          m.setSpeed(forward,forward,Setpoint_L,Setpoint_R);
+          /*********************
+          Serial.print("Setpoint_L: ");
+          Serial.print(Setpoint_L);
+          Serial.print("Setpoint_R: ");
+          Serial.print(Setpoint_R);
+          *********************/
+           Input_L= filter(10,0);
+           Input_R = filter(10,1);
+    //    Serial.print("    Input_L:");
+          Serial.println(Input_L);
+    //      Serial.print("    Input_R:");
+        Serial.println(Input_R);
+          delay(5000);
+          Setpoint_L=Setpoint_L+20;
+          Setpoint_R=Setpoint_R+20;
+          if(Setpoint_L>255||Setpoint_R>255)
+              Setpoint_R= Setpoint_L=70;
+  }
+}
+
 
 double filter(int n,int LR){
   sum=0;
